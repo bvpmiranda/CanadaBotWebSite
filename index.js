f0728baa00581a52c1c906cc272f4b6d6ea3046e
Bruno Miranda <bvpmiranda@mac.com>
2016-12-25 11:44:36 -0200

    test

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/ee', function(request, response) {
+  response.render('pages/expressEntry');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
