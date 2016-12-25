2e32993088c10d44ad44a8362800fa64ecffa982
Bruno Miranda <bvpmiranda@mac.com>
2016-12-25 11:40:12 -0200

    Tests

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+var app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error": message});
+}
+
+app.get("/question", function(req, res) {
+});
