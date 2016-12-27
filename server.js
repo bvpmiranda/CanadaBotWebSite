2cdbe3432939512d7118cf41d4cfb60cc57827b4
Bruno Miranda <bvpmiranda@mac.com>
2016-12-27 18:41:13 -0200

    test

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
-var calculator = require("/scripts/ee20161119");
+//var calculator = require("/scripts/ee20161119");
 
 var app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
 
 app.get("/expressEntry", function(req, res) {
     //res.status(200).json(calculator.test());
