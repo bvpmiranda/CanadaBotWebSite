f4d256125296007f502496e5497558f20300c450
Bruno Miranda <bvpmiranda@mac.com>
2016-12-27 20:16:08 -0200

    test

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
 
+
 var app = express();
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
@@ -12,13 +14,18 @@ function handleError(res, reason, message, code) {
   res.status(code || 500).json({"error": message});
 }
 
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 app.get("/expressEntry", function(req, res) {
-    //var calculatorExport = require("./calculator");
+    var calculatorExport = require("./calculator");
+    
     //var calculator = new calculatorExport();
 
     //res.status(200).json(calculator.test());
     
-    //res.status(200).json(calculator.exportedA);
+    res.status(200).json(calculatorExport.exportedA);
 
     res.status(200).json(req.query);
 });
