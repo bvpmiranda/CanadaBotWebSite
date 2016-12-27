5307b3b952a088b9cc240fb2eda91d3d1f7e9a36
Bruno Miranda <bvpmiranda@mac.com>
2016-12-27 19:01:15 -0200

    teste

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
-//var calculator = require("public/scripts/calculator");
+var calculator = require("calculator");
 var ObjectID = mongodb.ObjectID;
 
 var CONTACTS_COLLECTION = "contacts";
